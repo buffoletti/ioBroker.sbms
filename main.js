25236dc9849001c04d7d306ebd0cfee85cfa6d63
Sponkdamonk <piet.peterson@gmail.com>
2025-08-28 09:30:12 +0200

    rename

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require("@iobroker/adapter-core");
-const mqttHandler = require("./mqtt");
+const mqttHandler = require("./mqttHandler");
 const htmlHandler = require("./html");
 const { createNormalStates } = require("./states");
 const { createMqttDebugStates } = require("./mqttDebugStates");

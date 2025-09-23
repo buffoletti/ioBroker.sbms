cc0daf5459ea27b0d8821ec754502670478f5089
buffoletti <info@buffoletti.de>
2025-09-23 20:19:51 +0200

    debug

--- main.js
+++ main.js
@@ -48,19 +48,19 @@ class SbmsAdapter extends utils.Adapter {
 
         // serial enabled
         if (this.config.useSerial) {
-            serialHandler.init(this, this.config.debug);
+            serialHandler.init(this);
         } else {
             // serial not enabled
             this.setState("info.connection", { val: null, ack: true });
 
             // mqtt enabled
             if (this.config.useMQTT) {
-                mqttHandler.init(this, this.config.mqttTopic, this.config.debug);
+                mqttHandler.init(this, this.config.mqttTopic);
             }
 
             // html enabled
             if (this.config.useHtml) {
-                htmlHandler.init(this, this.config.debug);
+                htmlHandler.init(this);
             }
         }
     }
